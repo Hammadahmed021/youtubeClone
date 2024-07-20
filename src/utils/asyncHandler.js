b28450ca05f9cc207fa10abb675ae38a745dcb2d
HammadAhmed021 <hammadahmed4015@gmail.com>
2024-07-21 04:06:05 +0500

    after user route and controller

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,21 +1,23 @@
-const asyncHandler = (fn) => async (res, req, next) => {
-  try {
-    await fn(res, req, next);
-  } catch (error) {
-    res.status(error.code || 500).json({
-      success: false,
-      message: error.message,
-    });
+// / using Promise way
+const asyncHandler = (requestHandler) => {
+  return (res, req, next) => {
+    Promise.resolve(requestHandler(res, req, next)).catch((error) => next(error))
   }
-};
+}
 
-export { asyncHandler };
+export default asyncHandler;
 
 
-// using Promise way
-// const asyncHandler = (requestHandler) => {
-//   (res, req, next) => {
-//     Promise.resolve(requestHandler(res, req, next)).catch((error) => next(error))
+
+
+// const asyncHandler = (fn) => async (res, req, next) => {
+//   try {
+//     await fn(res, req, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
 //   }
-// }
+// };
 
